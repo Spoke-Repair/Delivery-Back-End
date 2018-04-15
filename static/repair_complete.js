5a88ee52c34d46e2160401bd2b7be79a720a5c40
Joshua Pham <joshua.pham@gmail.com>
2018-04-15 03:17:16 -0500

    date text changes when activeCustomer date is set

--- static/repair_complete.js
+++ static/repair_complete.js
@@ -1,7 +1,7 @@
 Vue.prototype.$eventHub = new Vue();
 
 Vue.component('customer-item', {
-    props: ['customer', 'activeCustomer'],
+    props: ['customer'],
     template: `<tr>
                 <td>{{customer.name}}</td>
                 <td>
@@ -15,14 +15,19 @@ Vue.component('customer-item', {
                </tr>`,
     methods: {
         'changeActiveCustomer': function() {
-            this.activeCustomer.name = this.customer.name;
+            this.$eventHub.$emit('activeCustomerNameChanged', this.customer.name)
         }
+    },
+    mounted: function() {
+        this.$eventHub.$on('activeCustomerDateChanged', function(activeCustomer) {
+            if (this.customer.name == activeCustomer.name)
+                this.customer.date = activeCustomer.date;
+        }.bind(this));
     }
 })
 
 
 Vue.component('customers', {
-    props: ['activeCustomer'],
     template: `<div id="customers">
                 <table class="table">
                     <thead>
@@ -33,7 +38,7 @@ Vue.component('customers', {
                         </tr>
                     </thead>
                     <tbody>
-                        <customer-item v-for="customer in customers" v-bind:customer="customer" v-bind:activeCustomer="activeCustomer">
+                        <customer-item v-for="customer in customers" v-bind:customer="customer">
                         </customer-item>
                     </tbody>
                 </table></div>`,
@@ -44,7 +49,6 @@ Vue.component('customers', {
     },
     mounted: function() {
         axios.get('/customer-data').then(function(res) {
-            // console.log(res)
             this.customers = res.data.map(function(row) {
                 return {
                     'name': row[0],
@@ -57,7 +61,6 @@ Vue.component('customers', {
 })
 
 Vue.component('date-update-modal', {
-    props: ['activeCustomer'],
     template: `<div class="modal fade" id="modal-datepicker" tabindex="-1" role="dialog" aria-hidden="true">
                   <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -72,13 +75,34 @@ Vue.component('date-update-modal', {
                       </div>
                       <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-primary" v-on:click="changeActiveCustomerDate">Save changes</button>
                       </div>
                     </div>
                   </div>
                 </div>`,
     mounted: function () {
-        $('#datepicker').datepicker()
+        $('#datepicker').datepicker({
+            onSelect: function(dateText, inst) {
+                this.activeCustomer.date = new Date(dateText);
+            }.bind(this)
+        });
+
+        this.$eventHub.$on('activeCustomerNameChanged', function(name) {
+            this.activeCustomer.name = name;
+        }.bind(this))
+    },
+    methods: {
+        'changeActiveCustomerDate': function() {
+            this.$eventHub.$emit('activeCustomerDateChanged', this.activeCustomer);
+        }
+    },
+    data: function() {
+        return {
+            'activeCustomer': {
+                'name': "",
+                'date': new Date()
+            }
+        }
     }
 })
 
@@ -87,7 +111,7 @@ var deliveryView = new Vue({
     template: '<div><date-update-modal v-bind:activeCustomer="activeCustomer"/><customers v-bind:activeCustomer="activeCustomer"/></div>',
     data: function() {
         return {
-            'activeCustomer': {'name': ""}
+            'activeCustomer': {'name': "", 'date': new Date()}
         }
     }
 })
